0282f15a273ab6c45eef948e483497a3a6b79893
Roman Nikitenko <Lomkaaa@ukr.net>
2022-10-19 07:12:17 +0300

    clone dia

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,24 @@
 'use strict';
+
+const arrRight = document.querySelector('.arrow__right');
+const arrLeft = document.querySelector('.arrow__left');
+const sliderLine = document.querySelector('.slider__line');
+let offset = 0;
+
+arrRight.addEventListener('click', () => {
+  offset += 1167;
+
+  if (offset > 2334) {
+    offset = 0;
+  }
+  sliderLine.style.left = -offset + 'px';
+});
+
+arrLeft.addEventListener('click', () => {
+  offset -= 1167;
+
+  if (offset < 0) {
+    offset = 2334;
+  }
+  sliderLine.style.left = -offset + 'px';
+});
